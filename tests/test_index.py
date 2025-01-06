946a8a3ffd61ec2705eea76febfd93541044b198
Rory <petpuppies@gmail.com>
2025-01-05 22:09:37 -0500

    started migrating playwright tests to python

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,3 +1,23 @@
-def test_index_loads(client):
-    response = client.get("/")
-    assert response.status_code == 200
+import re
+import os
+from playwright.sync_api import sync_playwright, Page, expect
+
+
+def test_has_title():
+    with sync_playwright() as p:
+        # Launch the browser
+        browser = p.chromium.launch(headless=False, slow_mo=1000, devtools=True)
+
+        # Create a browser context with basic authentication
+        context = browser.new_context(
+            http_credentials={"username": "misc", "password": "misc"}
+        )
+
+        # Open a new page in the authenticated context
+        page = context.new_page()
+
+        # Navigate to the URL requiring basic auth
+        page.goto(os.environ["BASE_URL"])
+
+        store_name = os.environ["STORE_NAME"]
+        expect(page).to_have_title(re.compile(store_name))
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,34 @@
-from flask_app.modules.extensions import DB
+import os
+import re
+import pytest
+from playwright.sync_api import sync_playwright, expect
 
 
-def test_login(app, client):
-    """Test the login route"""
-    with app.test_request_context():
-        response = client.post(
-            "/partials/login",
-            data={
-                "email": app.config.get("TEST_ACCOUNT_EMAIL"),
-                "password": app.config.get("TEST_ACCOUNT_PASSWORD"),
-            },
-        )
+@pytest.fixture(scope="session")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=100, devtools=True)
+        yield browser
+        # browser.close()
 
-        print(response.text)
-        assert response.status_code == 200
+
+@pytest.fixture(scope="function")
+def page(browser):
+    context = browser.new_context(
+        http_credentials={"username": "misc", "password": "misc"}
+    )
+    page = context.new_page()
+    yield page
+    context.close()
+
+
+def test_user_can_access_dashboard_after_login(page):
+    base_url = os.environ.get("BASE_URL")
+    page.goto(f"{base_url}/login")
+    page.locator("#email-entry-field").fill(os.environ.get("TEST_ACCOUNT_EMAIL"))
+    page.locator("#email-entry-field").press("Tab")
+    page.locator("#password-field").fill(os.environ.get("TEST_ACCOUNT_PASSWORD"))
+    page.locator("#login-submit-button").click()
+    page.wait_for_url("**/app")
+    expect(page).to_have_url(re.compile(r"/app"))
+    expect(page).to_have_title(re.compile(r"Dashboard"))
